58dec508475f73cc3d934d676a130e5a24bfbf5f
Chaoran Yu <yuchaoran2011@gmail.com>
2013-05-29 09:31:02 -0700

    proj2

---
+++ client2.c
@@ -0,0 +1,139 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <math.h>
+#include <limits.h>
+
+
+#define DEFAULT_INTERVAL 6
+#define DEFAULT_MSG "0"
+#define NUM_PACKETS 1000
+
+int R = DEFAULT_INTERVAL;
+int allowed_sending = 0;
+
+
+
+int poisson(float lamda) {
+	int k = 0;
+	float p = 1, L, u;
+	L= exp(-lamda);
+	while (1) {
+		k++;
+		u = (float) rand()/RAND_MAX;
+		p = p * u;
+		if (p < L) 
+			break;
+	}
+	return k-1;
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+	int socketFD, sockfd2;
+	int rate = DEFAULT_INTERVAL;
+	char msg[141];
+	
+	struct addrinfo hints;
+	struct addrinfo *servinfo;
+	
+	struct timespec sleepTime = {0, 100};
+
+	FILE *input = fopen("sss.jpg", "rb");
+
+
+	fseek(input, 0L, SEEK_END);
+ 	int size = ftell(input);
+ 	fseek(input, 0L, SEEK_SET);
+
+	
+	//Get sockets
+	socketFD = socket(AF_INET, SOCK_DGRAM, 0);
+	if (socket < 0){
+		printf("Error: Could not get socket.");
+		exit(1);
+	}
+	
+	//Get connection info
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_PASSIVE;
+	
+	if (getaddrinfo("10.10.66.90", "56790", &hints, &servinfo) != 0 || servinfo == NULL) {
+	    printf("Error: Could not find router.");
+	    exit(1);
+	}
+
+	
+
+	int count = 1;
+	int i;
+	double timestamp;
+	int tail = size % 128;
+
+
+	printf("%d\n", tail);
+
+	int a[32];
+	while(fread(&a, 32*sizeof(int), 1, input) && !feof(input)) {
+		sleepTime.tv_nsec = poisson((float)R)*1000000;
+		nanosleep(&sleepTime, NULL);
+		
+		memcpy(msg,a,128);
+		msg[128] = '2';   // flow number
+		memcpy(msg+129, &count, 4);
+		timestamp = clock()/(double)CLOCKS_PER_SEC;
+
+		memcpy(msg+133, &timestamp, 8);
+		
+
+		if (sendto(socketFD, msg, 141, 0, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
+			printf("Sending failed!\n");
+			exit(1);
+		}
+		
+		count += 1;
+	}
+
+
+
+	if (tail != 0) {
+		fread(&a, tail, 1, input);
+		sleepTime.tv_nsec = poisson((float)R)*1000000;
+		nanosleep(&sleepTime, NULL);
+		int temp = INT_MAX;
+		
+		memcpy(msg, &a, tail);
+		msg[128] = '1';   // flow number
+		memcpy(msg+129, &temp, 4);
+
+		memcpy(msg+133, &tail,4);
+		memcpy(msg+137, &count,4);
+
+		printf("%d\n",*(int *)(msg+133));
+		printf("%d\n",*(int *)(msg+137));
+
+		if (sendto(socketFD, msg, 141, 0, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
+			printf("Sending failed!\n");
+			exit(1);
+		}
+
+		printf("%d\n", tail);
+		
+	}
+
+	fclose(input);
+	
+	return 0;
+}
+	
+
---
+++ server2.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <limits.h>
+
+int main(int argc, char *argv[]){
+
+	int socketFD;
+	char buf[141];
+	int bytesReceived;
+	
+	struct addrinfo hints;
+	struct addrinfo *servinfo;
+	struct sockaddr_in clientinfo;
+	int clientinfolen;
+
+	FILE *output;
+	char fname[] = "sss_recv.jpg";
+	int a[32], count, tail, pos;
+
+	int packetsReceived1 = 0, packetsReceived2 = 0;
+
+	
+	//Get connection info
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_PASSIVE;
+
+
+	socketFD = socket(AF_INET, SOCK_DGRAM, 0);
+	if (socketFD < 0){
+		printf("Error: Could not get socket.\n");
+		exit(1);
+	}
+	
+	if(getaddrinfo("10.10.66.90", "56280", &hints, &servinfo) != 0 || servinfo == NULL){
+	    printf("Error: Could not get connection info with router.\n");
+	    exit(1);
+	}
+
+
+	if(bind(socketFD, servinfo->ai_addr, servinfo->ai_addrlen) < 0){
+		printf("Error: Could not bind socket.\n");
+		exit(1);
+	}
+
+	output = fopen(fname, "wb");
+
+	while (1) {
+
+		clientinfolen = sizeof(struct sockaddr_in);
+		if (recvfrom(socketFD, &buf, 141, 0, &clientinfo, &clientinfolen) == -1) {
+			printf("Receiving failed!\n");
+			exit(1);
+		}
+		
+		memcpy(a, buf, sizeof(int)*32);
+		memcpy(&count, buf+129, sizeof(int));
+
+		if (count != INT_MAX) {
+			fseek(output, 128*(count-1), SEEK_SET);
+			fwrite(&a, sizeof(int), 32, output);
+		} 
+		else {
+			tail = *(int *)(buf+133);
+			pos = *(int *)(buf+137);
+			printf("%d\n",tail);
+			printf("%d\n",pos);
+			fseek(output, 128*(pos-1), SEEK_SET);
+			fwrite(&a, sizeof(char), tail, output);
+			fclose(output);
+		}
+
+		packetsReceived1 += 1;
+		printf("Flow: %c\n", buf[128]);
+		printf("Count: %d\n", *(int *)(buf+129));
+		printf("Time Stamp: %lf\n", *(double *)(buf+133));
+		printf("%d\n", packetsReceived1);
+		printf("\n");
+	}	
+}
+
+	
+	
+
